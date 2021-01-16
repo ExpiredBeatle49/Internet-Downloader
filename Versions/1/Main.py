35117aac619afc8b5df7dd9eb5672086528e3c70
ExpiredBeatle49 <60576955+ExpiredBeatle49@users.noreply.github.com>
2021-01-16 14:42:52 -0500

    Delete Main.py

--- Versions/1/Main.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-
-a = input("Enter the url: ")
-print('Beginning file download...')
-urllib.request.urlretrieve(a, 'C:\Users\moran\OneDrive\Desktop\Python\Internet-Downloader\Downloads\test.jpg')
-print('Done')
-exit()
