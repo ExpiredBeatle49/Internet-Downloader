e6a6215fdf6d1863eb477185f363618d7c10f93f
ExpiredBeatle49 <60576955+ExpiredBeatle49@users.noreply.github.com>
2021-01-16 15:09:12 -0500

    E

--- Setup.py
+++ Setup.py
@@ -4,6 +4,7 @@ import os
 
 sg.theme('dark grey 9')
 
+Link = 'https://pastebin.com/raw/TrhGpS3E'
 paste = 'https://pastebin.com/raw/Xa2Ea4rD'
 CURR_DIR = os.path.dirname(os.path.realpath(__file__))
 
@@ -15,6 +16,7 @@ window = sg.Window('Setup', layout)
 event, values = window.read()
 
 window['-Download-'].update("Downloading files...")
-os.mkdir(CURR_DIR + "/Downloads")
-wget.download(paste, str(CURR_DIR) + "/Main.py")
+os.mkdir(str(CURR_DIR) + "/Downloads")
+wget.download(Link, str(CURR_DIR) + "\Downloads\Link Examples.txt")
+wget.download(paste, str(CURR_DIR) + "\Main.py")
 window['-Download-'].update("Done, you may now close the window.")
